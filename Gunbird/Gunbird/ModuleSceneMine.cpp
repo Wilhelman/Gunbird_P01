4541764ecf1adb3d6301089427060d6a0e4aab87
Wilhelman <guillermogarsub@gmail.com>
2017-03-29 20:12:46 +0200

    Mine details

--- Gunbird/Gunbird/ModuleSceneMine.cpp
+++ Gunbird/Gunbird/ModuleSceneMine.cpp
@@ -3,73 +3,74 @@
 #include "ModuleTextures.h"
 #include "ModuleRender.h"
 #include "ModuleAudio.h"
-#include "ModuleSceneSea.h"
 #include "ModuleSceneMine.h"
 #include "ModulePlayer.h"
 #include "ModuleFadeToBlack.h"
+#include "ModuleScoreRanking.h"
 
 //TODO: remove this if not necesary
 #include "ModuleInput.h"
 
-
-ModuleSceneSea::ModuleSceneSea()
+ModuleSceneMine::ModuleSceneMine()
 {
 	// Background
 	background_x = 0;
-	background_y = -2036;
+	background_y = -3535;
 	background.w = SCREEN_WIDTH;
-	background.h = 2036;
+	background.h = 3535;
 }
 
-ModuleSceneSea::~ModuleSceneSea()
+ModuleSceneMine::~ModuleSceneMine()
 {}
 
 // Load assets
-bool ModuleSceneSea::Start()
+bool ModuleSceneMine::Start()
 {
-	LOG("Loading SceneSea assets");
+	LOG("Loading SceneMine assets");
 	bool ret = true;
-	background_y = -2036;
+
+	background_y = -3535;
+
 	App->player->Enable();
 
-	graphics = App->textures->Load("Assets/maps/map_sea_background.png");
+	graphics = App->textures->Load("Assets/maps/map_mine_background.png");
 	if (graphics == nullptr) {
-		LOG("Cannot load the texture in SceneSea");
+		LOG("Cannot load the texture in SceneMine");
 		ret = false;
 	}
-	if (!App->audio->PlayMusic("Assets/audio/gunbird_sea_level_music.wav"))
+	if (!App->audio->PlayMusic("Assets/audio/gunbird_mine_level_music.wav"))
 		ret = false;
 	return ret;
 }
 
 // Update: draw background
-update_status ModuleSceneSea::Update()
+update_status ModuleSceneMine::Update()
 {
 
 	update_status status = UPDATE_CONTINUE;
 
 	int speed = 1;
-	if(background_y< -SCREEN_HEIGHT)
+	if (background_y< -SCREEN_HEIGHT)
 		background_y += speed;
 
 	// Draw everything --------------------------------------
 	if (!App->render->Blit(graphics, background_x, background_y + SCREEN_HEIGHT, &background, 0.75f)) {
-		LOG("Cannot blit the texture in Scene Sea %s\n",SDL_GetError());
+		LOG("Cannot blit the texture in Scene Mine %s\n", SDL_GetError());
 		status = UPDATE_ERROR;
 	}
 
-	//TODO change the position of the player to private to be more pro
+	//TODO change the position of the player to private to do it better
 
-	if ((App->player->position.y < 0 && App->fade->FadeIsOver()) ||/*TODO: remove this condition*/ (App->input->keyboard[SDL_SCANCODE_RETURN] && App->fade->FadeIsOver()))
-		App->fade->FadeToBlack(this, App->sceneMine);
+	if ((App->player->position.y < 0 && App->fade->FadeIsOver()) || App->input->keyboard[SDL_SCANCODE_RETURN] && App->fade->FadeIsOver())
+		App->fade->FadeToBlack(this, App->scoreRanking);
 
 	return status;
 }
 
-bool ModuleSceneSea::CleanUp()
+bool ModuleSceneMine::CleanUp()
 {
 	//TODO check if we can control this
-	LOG("Unloading Scene Sea");
+	LOG("Unloading Scene Mine");
 
 	App->player->Disable();
 	graphics = nullptr;
