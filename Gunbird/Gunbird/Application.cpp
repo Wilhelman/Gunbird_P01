617fac39f3d2c60dfc4ffb79d3358bea73350728
Wilhelman <guillermogarsub@gmail.com>
2017-04-20 20:33:05 +0200

    Space in Application module

--- Gunbird/Gunbird/Application.cpp
+++ Gunbird/Gunbird/Application.cpp
@@ -86,7 +86,7 @@ update_status Application::Update()
 
 	for (int i = 0; i < NUM_MODULES && ret == UPDATE_CONTINUE; ++i)
 		ret = modules[i]->IsEnabled() ? modules[i]->PostUpdate() : UPDATE_CONTINUE;
-
+	 
 	return ret;
 }
 
