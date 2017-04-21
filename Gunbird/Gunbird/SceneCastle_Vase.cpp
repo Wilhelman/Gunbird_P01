feb24f0fcba4610f811c0247cc4892ccc54a2436
martimab <martialtimira@gmail.com>
2017-04-21 10:32:35 +0200

    Vase Hit animation

--- Gunbird/Gunbird/SceneCastle_Vase.cpp
+++ Gunbird/Gunbird/SceneCastle_Vase.cpp
@@ -1,15 +1,26 @@
 #include "Application.h"
 #include "SceneCastle_Vase.h"
 #include "ModuleCollision.h"
+#include "ModuleParticles.h"
+#include "SDL\include\SDL_timer.h"
+#include "ModuleCollision.h"
 #include "Path.h"
 
 
 SceneCastle_Vase::SceneCastle_Vase(int x, int y) : Enemy(x, y)
 {
+	lives = 11;
 
 	vase.PushBack({ 552,578,33,49 });
 	vase.loop = true;
 
+	/*vaseRed.PushBack({ 552,578,33,49 });
+	vaseRed.PushBack({ 552,578,33,49 });
+	vaseRed.PushBack({ 552,578,33,49 });
+	vaseRed.PushBack({ 552,578,33,49 });*/
+	
+	vaseWhite.PushBack({ 593,579,33,49 });
+	vaseWhite.loop = false;
 
 	animation = &vase;
 
@@ -22,7 +33,24 @@ SceneCastle_Vase::SceneCastle_Vase(int x, int y) : Enemy(x, y)
 void SceneCastle_Vase::Move()
 {
 	position = original_pos + movement.GetCurrentPosition(&animation);
+
+	if (animation == &vaseWhite && lastTime == 0) {
+		lastTime = SDL_GetTicks();
+	}
+
+	currentTime = SDL_GetTicks();
+
+	if (currentTime > (lastTime + 70)) {
+		animation = &vase;
+		lastTime = 0;
+	}
 }
 void SceneCastle_Vase::OnCollision(Collider* collider) {
-
+	if (collider->type == COLLIDER_TYPE::COLLIDER_PLAYER_SHOT) {
+		animation = &vaseWhite;
+		lives--;
+	}
+}
+uint SceneCastle_Vase::getLives() {
+	return lives;
 }
