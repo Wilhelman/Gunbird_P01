e6f0e996e5d791e410a56a7cb82dc0c466a95336
Wilhelman <guillermogarsub@gmail.com>
2017-06-03 11:30:02 +0200

    Starting boss :boar:

---
+++ Gunbird/Gunbird/Enemy_Mecha_Boss.h
@@ -0,0 +1,35 @@
+#ifndef __Enemy_Mecha_Boss_H__
+#define __Enemy_Mecha_Boss_H__
+
+#include "Enemy.h"
+#include "Path.h"
+#include "ModuleParticles.h"
+
+class Enemy_Mecha_Boss : public Enemy
+{
+private:
+	bool left;
+
+	Animation idle;
+	Animation hitWhite;
+	Animation redNormal;
+	Animation redNormal_2;
+	Animation redNormal_3;
+	iPoint original_pos;
+
+	uint lastShot;
+
+	Particle lastParticle;
+
+public:
+
+	Enemy_Mecha_Boss(int x, int y);
+
+	void Move();
+	void Shoot();
+
+	void OnCollision(Collider* collider);
+	uint getLives();
+};
+
+#endif
